8ff4ef6ca8d753c19111ad55dc90edfd4847ea72
Dean <349321810@qq.com>
2015-04-29 22:02:17 +0800

    1111

--- luaPatch/HomeViewController.lua
+++ luaPatch/HomeViewController.lua
@@ -5,12 +5,13 @@ waxClass{"HomeViewController", UIViewController}
 
 function homeItemViewAction(self, itemView)
     local num = itemView:item():tag()
-    puts(num)
+    puts(type(num)..num)
 
-    if (num == 1000) then
+    if (num == 1100) then
          self:adsJump()
-    else if (num == 1200) then
+    elseif (num == 1200) then
          self:mainPageController():goModule(MainTypeHotel)
+    else
     end
 
 end
